fb7adc00f96683ed36e2f75a1a53dcfb70bbb666
aavetis <albert.avetisian@gmail.com>
2023-10-10 19:55:53 -0400

    updates to examples

--- examples/langchain-example.py
+++ examples/langchain-example.py
@@ -7,7 +7,10 @@ from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 # add your APIM hostname as base url
 BASE_URL = "https://openai-api-XXXXXXXX.azure-api.net"
 
+# name of your chat model deployment in Azure OpenAI
 CHAT_DEPLOYMENT = "my-az-openai-chat-deployment-name"
+
+# name of your embedding model deployment in Azure OpenAI
 EMBEDDING_DEPLOYMENT = "my-az-openai-ada-deployment-name"
 
 # be sure to inject your Subscription key as API key, or set it as an env var:
--- examples/openai-example.py
+++ examples/openai-example.py
@@ -1,12 +1,19 @@
 import os
 import openai
 
+
 openai.api_type = "azure"
-openai.api_key = os.getenv("OPENAI_API_KEY")
-openai.api_base = "https://openai-api-XXXXXX.azure-api.net"
-openai.api_version = "2023-07-01-preview"
+
+# be sure to inject your Subscription key as API key, or set it as an env var:
+# `export OPENAI_API_KEY=YOUR_KEY`
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+# add your APIM hostname as base url
+openai.api_base = "https://openai-api-XXXXXX.azure-api.net"
+
+openai.api_version = "2023-07-01-preview"
+
+# standard ChatCompletion example
 response = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
     messages=[
